5fd0843c5003c99e17d4b345e5423f1b1d5a3d42
Beat Besmer <beat.besmer@gmail.com>
2024-12-16 21:46:13 +0100

    Add puma and nginx config file templates and add a pumactl binstub

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, 'yglow'
-set :repo_url, 'git@github.com:besi/ideahub'
+set :repo_url, 'git@github.com:besi/yglow'
 
 set :log_level, :error
 set :bundle_binstubs, nil
@@ -9,12 +9,18 @@ set :linked_files, %w{config/database.yml .env.production puma.rb}
 set :rbenv_prefix, '/usr/bin/rbenv exec'
 
 namespace :deploy do
+
+  # start the server via root
+  # systemctl start puma_yglow_production
+
   desc 'Restart application'
   task :restart do
     on roles(:app) do
-      execute "#{fetch(:rbenv_prefix)} pumactl -P ~/rails/current/tmp/pids/puma.pid phased-restart"
+      within release_path do
+        execute :pumactl, '-F', '~/rails/shared/puma.rb', 'phased-restart'
+      end
     end
   end
 end
 
-# after 'deploy:publishing', 'deploy:restart'
+after 'deploy:publishing', 'deploy:restart'
